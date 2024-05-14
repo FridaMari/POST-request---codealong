fb83f6451dc3c3c00994686db678f3e236389a5d
Frida Svensson <fridamarisvensson@gmail.com>
2024-05-14 20:51:43 +0200

    Code along with Damien when he talks about POST requests

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/notes";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// Note model
+const Note = mongoose.model("Note", {
+  text: String,
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -14,6 +28,13 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+// Post handler
+app.post("/notes", async (req, res) => {
+  const note = new Note({ text: req.body.text }); //text: req.body.text to prevent user to set creation date
+  await note.save();
+  res.json(note);
 });
 
 // Start the server
